e4a5de62e663e72bbb7cfb8b490f8777648e3fa0
Nicsi <nicolas.sirri@edu.esiee.fr>
2018-06-03 15:27:15 +0200

    vue pizzas - fonctionnelle

--- src/view/Pizzas.java
+++ src/view/Pizzas.java
@@ -1,17 +1,34 @@
 package view;
 import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTable;
 
 import controller.ViewController;
+import metier.Pizza;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
 import javax.swing.JTree;
 import javax.swing.tree.DefaultTreeModel;
+
+import DAO.DAOPizza;
+
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.Point;
+
+import javax.swing.SwingConstants;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.JTextPane;
 
 
 
@@ -20,21 +37,31 @@ public class Pizzas extends JPanel {
 	private static final long serialVersionUID = 1L;
 	JLabel lblInformations;
 	JLabel lblNom;
-	JLabel lblIngrdients;
+	JTextPane lblIngrdients = new JTextPane();
 	JLabel lblTarifs;
 	JLabel lblNaine;
 	JLabel lblHumaine;
 	JLabel lblOgresse;
 	JTree ArbrePizzas;
+	JLabel lblPrixNaine = new JLabel("");
+	JLabel lblPrixHumaine = new JLabel("");
+	JLabel lblPrixOgresse = new JLabel("");
+	Vector<Pizza> allPizzas = null;
+	int[] startIndex = new int[1];
+	Map<DefaultMutableTreeNode,Integer> nodeOrder = new HashMap<DefaultMutableTreeNode,Integer>() ;
 	
+
 	public Pizzas(MainFrame menu) {
 		
+		startIndex[0] = 1;
 		setBackground(Color.LIGHT_GRAY);
 		setBounds(100,100,720,423);
 		CreatePizzaTree();
 		DisplayPizzaInfos();
-		OnSelectionShowPizzaInfos(0);
+		OnSelectionShowPizzaInfos(startIndex);
 		Button_Return();
+	
+		
 			
 	}
 	
@@ -49,20 +76,57 @@ public class Pizzas extends JPanel {
 		});
 		setLayout(null);
 		add(btnReturn);
-		btnReturn.setFocusPainted(false);	
+		btnReturn.setFocusPainted(false);
 		
+
+		
+		
+	
 	}
 
 
-	
-	//TODO Au clic sur un element du Jtree afficher les caract de la pizza dans les labels correspondants (nom + ingrédients + tarif naine/humaine/ogresse)
-	// Par défault choisir la pizza 0
-	void OnSelectionShowPizzaInfos(int index)
+	void OnSelectionShowPizzaInfos(int index[])
 	{
+		if(index[0] != 0)
+		{
+		lblNom.setText("Nom : " + allPizzas.get(index[0]-1).nom);
 		
+		lblIngrdients.setText("Ingrédients : ");
+		for(int i = 0; i < allPizzas.get(index[0]-1).ingredients.size(); i++)
+		{
+			
+			lblIngrdients.setText(lblIngrdients.getText() + allPizzas.get(index[0]-1).ingredients.get(i).nom + ", " );
+		}
+		DecimalFormat df = new DecimalFormat("##.00");
+		lblPrixNaine.setText(String.valueOf(df.format(allPizzas.get(index[0]-1).base_prix * 0.66f)));
+		lblPrixHumaine.setText(String.valueOf(df.format(allPizzas.get(index[0]-1).base_prix)));
+		lblPrixOgresse.setText(String.valueOf(df.format(allPizzas.get(index[0]-1).base_prix * 1.33f)));
+		}
 	}
 	
 	void DisplayPizzaInfos() {
+		
+		lblPrixNaine.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPrixNaine.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPrixNaine.setBounds(315, 315, 66, 20);
+		add(lblPrixNaine);
+		
+		lblIngrdients.setBackground(Color.LIGHT_GRAY);
+		lblIngrdients.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblIngrdients.setBounds(180, 136, 520, 98);
+		add(lblIngrdients);
+		
+		lblPrixHumaine.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPrixHumaine.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPrixHumaine.setBounds(420, 315, 66, 20);
+		add(lblPrixHumaine);
+		
+		
+		lblPrixOgresse.setHorizontalAlignment(SwingConstants.CENTER);
+		lblPrixOgresse.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblPrixOgresse.setBounds(510, 315, 66, 20);
+		add(lblPrixOgresse);
+		
 		
 		lblInformations = new JLabel("Informations :");
 		lblInformations.setFont(new Font("Tahoma", Font.PLAIN, 24));
@@ -71,47 +135,68 @@ public class Pizzas extends JPanel {
 		
 		lblNom = new JLabel("Nom :");
 		lblNom.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNom.setBounds(180, 78, 46, 14);
+		lblNom.setBounds(180, 78, 412, 30);
 		add(lblNom);
-		
-		lblIngrdients = new JLabel("Ingr\u00E9dients :");
-		lblIngrdients.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblIngrdients.setBounds(180, 147, 151, 20);
-		add(lblIngrdients);
 		
 		lblTarifs = new JLabel("Tarifs :");
 		lblTarifs.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblTarifs.setBounds(180, 270, 51, 20);
+		lblTarifs.setBounds(180, 310, 51, 20);
 		add(lblTarifs);
 		
 		lblNaine = new JLabel("Naine");
 		lblNaine.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblNaine.setBounds(330, 241, 51, 20);
+		lblNaine.setBounds(330, 281, 51, 20);
 		add(lblNaine);
 		
 		lblHumaine = new JLabel("Humaine");
 		lblHumaine.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblHumaine.setBounds(420, 241, 82, 20);
+		lblHumaine.setBounds(420, 281, 82, 20);
 		add(lblHumaine);
 		
 		lblOgresse = new JLabel("Ogresse");
 		lblOgresse.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		lblOgresse.setBounds(510, 241, 82, 20);
+		lblOgresse.setBounds(510, 281, 82, 20);
 		add(lblOgresse);
 	}
 	
 	void CreatePizzaTree()
 	{
 		ArbrePizzas = new JTree();
+		ArbrePizzas.addTreeSelectionListener(new TreeSelectionListener() {
+			public void valueChanged(TreeSelectionEvent arg0) {
+				
+				
+        
+        		OnSelectionShowPizzaInfos(ArbrePizzas.getSelectionRows());
+
+        		
+			}
+		});
+		try {
+			allPizzas = DAOPizza.getThatDAO().GetAll();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		ArbrePizzas.setModel(new DefaultTreeModel(
 			new DefaultMutableTreeNode("Liste des pizzas") {
+				/**
+				 * 
+				 */
+				private static final long serialVersionUID = -6921231561470139968L;
+
 				{
-					add(new DefaultMutableTreeNode("TODO"));
-					add(new DefaultMutableTreeNode(""));
+					for(int i = 0; i<allPizzas.size();i++)
+					{
+						DefaultMutableTreeNode dmtn = new DefaultMutableTreeNode(allPizzas.get(i).nom);
+						add(dmtn);
+						
+					}
+					
 				}
 			}
 		));
-		ArbrePizzas.setBounds(0, 0, 141, 420);
+		ArbrePizzas.setBounds(0, 0, 160, 420);
 		add(ArbrePizzas);
 	}
 }
